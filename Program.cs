df2b8fa1f6bdaa4b50e1d601f50b09519f790279
Nii-Quaye <niiquaye.ashie.7@gmail.com>
2023-01-22 21:26:16 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        int age;
+        Console.WriteLine("Enter your age");
+        age =
+        Convert.ToInt16(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are old enough");
+        }
+        else if (age < 18)
+        {
+            Console.WriteLine("You are a kid");
+        }
+    }
+        }
